23cc0a6a4830933558b6c752da1ee91871fe9d45
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-03 08:56:57 -0600

    add first and second number

--- SimpleCalculator.vb
+++ SimpleCalculator.vb
@@ -1,7 +1,29 @@
 ﻿Module SimpleCalculator
 
     Sub Main()
+        Dim userMessage As String
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
 
+
+        Console.WriteLine("Choose a number:")
+        userMessage = Console.ReadLine()
+        firstNumber = CInt(userMessage)
+
+
+        Console.WriteLine("Choose a number:")
+        userMessage = Console.ReadLine()
+        secondNumber = CInt(userMessage)
+
+
+        Console.WriteLine("Choose one of the following options:")
+        Console.WriteLine("1. Sum")
+        Console.WriteLine("2. Product")
+        Console.WriteLine("3. Difference")
+        Console.WriteLine("4. Quotient")
+
+
+        Console.ReadLine()
     End Sub
 
 End Module
